037847f199cc1bf415984334b0e85d9683c224e8
Sherwyn Goh <sherwyn.lj.goh@gmail.com>
2016-10-19 21:45:28 +0800

    add favicon

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-main',
   template: `
   <h1>{{title}}</h1>
+  <div class="pace pace-activity"></div>
   <pokemon-master></pokemon-master>
   `
 })
